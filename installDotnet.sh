5432684ce4148e41409db33897ceae33f2411afa
Bisam97 <bisam97@bisam97.de>
2019-05-06 23:06:02 +0200

    adding projekt files

---
+++ installDotnet.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget -q https://packages.microsoft.com/config/ubuntu/18.10/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install aspnetcore-runtime-2.2
